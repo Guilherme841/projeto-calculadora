28d70cf808b56957685c1fd6c75d16fcec7dcc28
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2023-02-04 23:26:07 -0300

    Update

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
-function tecla7() {
-    document.getElementById('display').value += 7;
-}
+const botoes = document.querySelectorAll('.botao');
+const operadores = document.querySelectorAll('.operaçao');
+const display = document.getElementById('display');
+
+for (const button of botoes) {
+    button.addEventListener('click', function() {
+        let valorInput = this.value;
+        arrValues = [];
+        arrValues = document.getElementById('display').value += valorInput;
+        // console.log(arrValues);
+    });
+};
+
+for (const operador of operadores) {
+    operador.addEventListener('click', function() {
+        let valorInput = this.value;
+        if (valorInput === '+') {
+            document.getElementById('display').value = '';
+            
+        }
+    });
+};
