29ce6a53c1e006ec5b038d8eec2f66b01404d164
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2023-02-11 15:59:59 -0300

    Update

--- node_869477984e5e9.tmp.js
+++
@@ -1,8 +0,0 @@
-arr1 = [2,2,2,2,2];
-
-for (let i = 0; arr1.length; i++) {
-    soma = 0;
-    soma += arr1[i];
-    console.log(soma)
-}
-
--- scripts/script.js
+++ scripts/script.js
@@ -11,22 +11,52 @@ for (const button of botoes) {
     });
 };
 
-for (const operador of operadores) {
-    operador.addEventListener('click', function() {
-        if (this.value === '+') {
-            display.value = '';
-            arrValues1 = arrValues;
-            arrValues2 = arrValues1.join('');
-            arrValues4 = Number(arrValues2)
-            arrValues3 = arrValues.join('');
-            arrValues5 = Number(arrValues3)
-            arrValues = [];
-        }
-        if (this.value === '=') {
-            let soma = 0;
-            soma = arrValues4 + arrValues5;
-            display.value = soma;
-        }
-        
-    });
-};
+// for (const operador of operadores) {
+//     operador.addEventListener('click', function() {
+        // if (this.value === '+') {
+        //     display.value = '';
+        //     arrValues1 = arrValues;
+        //     arrValues2 = arrValues1.join('');
+        //     arrValues = [];
+        // }
+        // if (this.value === '=') {
+        //     arrValues4 = Number(arrValues2)
+        //     arrValues3 = arrValues.join('');
+        //     arrValues5 = Number(arrValues3)
+        //     let soma = 0;
+        //     soma = arrValues4 + arrValues5;
+        //     display.value = soma;
+        // }
+//     });
+// };
+
+// Seleção de operadores/funções/eventos 
+
+const adiçao = document.getElementById('operaçaoAdiçao');
+const sub = document.getElementById('operaçaoSub');
+const igual = document.getElementById('operaçaoIgual');
+const divisao = document.getElementById('operaçaoDivisao');
+const porcent = document.getElementById('operaçaoPorcetagem');
+const multi = document.getElementById('operaçaoMulti');
+
+adiçao.addEventListener('click', operaçaoAdiçao);
+sub.addEventListener('click', sub);
+igual.addEventListener('click', igual);
+divisao.addEventListener('click', divisao);
+porcent.addEventListener('click', porcent);
+multi.addEventListener('click', multi);
+
+function operaçaoAdiçao() {
+    display.value = '';
+    arrValues1 = arrValues;
+    arrValues2 = arrValues1.join('');
+    arrValues = [];
+    if (this.value === '=') {
+        arrValues4 = Number(arrValues2)
+        arrValues3 = arrValues.join('');
+        arrValues5 = Number(arrValues3)
+        let soma = 0;
+        soma = arrValues4 + arrValues5;
+        display.value = soma;
+    }
+}
