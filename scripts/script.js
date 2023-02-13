a5742fc5cd2db16c67d53933ffe0dc0ffe77424f
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2023-02-13 01:37:45 -0300

    tradutor

--- scripts/script.js
+++ scripts/script.js
@@ -5,6 +5,7 @@ const display = document.getElementById('display');
 let arrValues1 = [];
 let arrValues2 = [];
 let operator = '';
+let result = 0;
 
 for (const button of botoes) {
     button.addEventListener('click', function() {
@@ -14,59 +15,60 @@ for (const button of botoes) {
         } else {
             arrValues2.push(this.value);
             display.value += this.value;
-        }
+        }        
     });
 };
 
 for (const operador of operadores) {
     operador.addEventListener('click', function() {
+        values1 = Number(arrValues1.join(''));
+        values2 = Number(arrValues2.join(''));
+
         if (this.value === '+') {
             operator = '+';
             display.value = '';
         } if (this.value === '=' && operator === '+') {
             let soma = 0;
-            values1 = Number(arrValues1.join(''));
-            values2 = Number(arrValues2.join(''));
             soma = values1 + values2;
             display.value = soma;
+            result = soma;
+            arrValues1 = [];
+            arrValues2 = [];
+            operator = '';
+
         } if (this.value === '-') {
             operator = '-';
             display.value = '';
         } if (this.value === '=' && operator === '-') {
             let sub = 0;
-            values1 = Number(arrValues1.join(''));
-            values2 = Number(arrValues2.join(''));
             sub = values1 - values2;
             display.value = sub;
+            result = sub;
+            arrValues1 = [];
+            arrValues2 = [];
+            operator = '';
+        } if (this.value === '/') {
+            operator = '/';
+            display.value = '';
+        } if (this.value === '=' && operator === '/') {
+            let divisao = 0;
+            divisao = values1 / values2;
+            display.value = divisao.toFixed(1);
+            console.log(divisao);
+        } if (this.value === '%') {
+            operator = '%';
+            display.value = '';
+        } if (this.value === '=' && operator === '%') {
+            let porcent = 0;
+            porcent = values1 % values2
+            display.value = porcent
+        } if (this.value === 'X') {
+            operator = 'X';
+            display.value = '';
+        } if (this.value === '=' && operator === 'X') {
+            let multi = 0;
+            multi = values1 * values2
+            display.value = multi
         }
     });
 };
-
-
-// const adiçao = document.getElementById('operaçaoAdiçao');
-// const sub = document.getElementById('operaçaoSub');
-// const igual = document.getElementById('operaçaoIgual');
-// const divisao = document.getElementById('operaçaoDivisao');
-// const porcent = document.getElementById('operaçaoPorcetagem');
-// const multi = document.getElementById('operaçaoMulti');
-
-// adiçao.addEventListener('click', operaçaoAdiçao);
-// sub.addEventListener('click', sub);
-// igual.addEventListener('click', igual);
-// divisao.addEventListener('click', divisao);
-// porcent.addEventListener('click', porcent);
-// multi.addEventListener('click', multi);
-
-// function operaçaoAdiçao() {
-//     operador = '+';
-//     display.value = '';
-//     let soma = 0;
-//     values1 = Number(arrValues1.join(''));
-//     values2 = Number(arrValues2.join(''));
-//     soma = values1 + values2;
-//     display.value = soma;
-//     console.log(values1);
-//     console.log(values2);
-//     console.log(soma);
-//     console.log(operador)
-// }
